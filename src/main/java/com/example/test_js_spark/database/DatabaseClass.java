62a92d322b4d75ab4e793e2c0809202b4dc11ec7
Olga Koltsova <hotolla@gmail.com>
2023-09-12 13:02:30 +0200

    initial commit

---
+++ src/main/java/com/example/test_js_spark/database/DatabaseClass.java
@@ -0,0 +1,7 @@
+package com.example.test_js_spark.database;
+
+public class DatabaseClass {
+    public int methodName(int a, int b) {
+        return a * b;
+    }
+}
---
+++ src/test/java/com/example/test_js_spark/TestJsSparkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_js_spark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestJsSparkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
